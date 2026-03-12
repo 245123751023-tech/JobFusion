b88ad204b7cce28e756ddc6ab5cb96fc9005e18b
PraveenAchary <praveenvishwabramham@gmail.com>
2026-03-12 23:44:54 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,20 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, db, storage
 
-cred = credentials.Certificate(r"E:\Django\jobfusion\config\fir-chat-e3c87-firebase-adminsdk-fbsvc-0eba3f02b8.json")
+firebase_cred_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+firebase_cred_path = os.environ.get("FIREBASE_CREDENTIALS_PATH")
+
+if firebase_cred_json:
+    cred = credentials.Certificate(json.loads(firebase_cred_json))
+elif firebase_cred_path:
+    cred = credentials.Certificate(firebase_cred_path)
+else:
+    cred = credentials.Certificate(r"E:\Django\jobfusion\config\fir-chat-e3c87-firebase-adminsdk-fbsvc-0eba3f02b8.json")
 
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://fir-chat-e3c87-default-rtdb.asia-southeast1.firebasedatabase.app',
+    'databaseURL': os.environ.get("FIREBASE_DATABASE_URL", "https://fir-chat-e3c87-default-rtdb.asia-southeast1.firebasedatabase.app"),
 })
 
 firebase_db = db.reference()
