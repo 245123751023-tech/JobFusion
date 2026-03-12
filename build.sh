f6e69d8c1cb2a1bb2501733ce7825a8f949e389a
Nenu Cheppanu <frontendbackend36@gmail.com>
2026-03-12 21:08:37 +0530

    fixed build script

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
-==> Cloning from https://github.com/PraveenAchary/JobFusion
-==> Checking out commit 1e860e7a8c898307edd4bd472842ff052678e70a in branch main
-==> Installing Python version 3.14.3...
-==> Using Python version 3.14.3 (default)
-==> Docs on specifying a Python version: https://render.com/docs/python-version
-==> Using Poetry version 2.1.3 (default)
-==> Docs on specifying a Poetry version: https://render.com/docs/poetry-version
-==> Running build command './build.sh'...
-bash: line 1: ./build.sh: No such file or directory
-==> Build failed 😞
-==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
