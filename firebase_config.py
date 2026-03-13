34038d4e86155e840cc984fae28ee8f264dec4ef
PraveenAchary <praveenvishwabramham@gmail.com>
2026-03-13 10:32:50 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -11,13 +11,10 @@ if firebase_cred_json:
 elif firebase_cred_path:
     cred = credentials.Certificate(firebase_cred_path)
 else:
-    cred = credentials.Certificate("config/fir-chat-e3c87-firebase-adminsdk-fbsvc-0eba3f02b8.json")
+    cred = credentials.Certificate(r"E:\Django\jobfusion\config\fir-chat-e3c87-firebase-adminsdk-fbsvc-0eba3f02b8.json")
 
 firebase_admin.initialize_app(cred, {
-    'databaseURL': os.environ.get(
-        "FIREBASE_DATABASE_URL",
-        "https://fir-chat-e3c87-default-rtdb.asia-southeast1.firebasedatabase.app"
-    ),
+    'databaseURL': os.environ.get("FIREBASE_DATABASE_URL", "https://fir-chat-e3c87-default-rtdb.asia-southeast1.firebasedatabase.app"),
 })
 
 firebase_db = db.reference()
